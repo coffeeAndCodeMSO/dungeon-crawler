8f0fe78b28cd1889499221d8c7c587240ae3f460
ed42311 <ed42311@gmail.com>
2017-09-18 18:16:29 -0600

    remove outside layer

---
+++ src/components/theHero.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../styles/TheHero.css";
+
+export default class TheHero extends Component {
+  render() {
+    return(
+      <div className="TheHero">
+      </div>
+    );
+  }
+}
